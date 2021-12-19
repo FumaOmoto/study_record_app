8c1f932701871dcd8aa5b498af8ba56fc3fce7c4
huna112 <bokobokokumasan@gmail.com>
2021-12-19 10:59:26 +0900

    posts showページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
 
+  def show 
+    @user = current_user if logged_in?
+    @post = current_user.posts.find_by(id: params[:id])
+  end
+
   def new
     @user = current_user
     @post_create = current_user.posts.build
